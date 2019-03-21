acc728764909fc1a462ed5a4da48ec832cad1d5d
Mariana <lustosamariana67@gmail.com>
2019-03-21 20:49:01 -0300

    JS básico

---
+++ javascript.js
@@ -0,0 +1,31 @@
+// var order = ["morango", "banana", "manga", "uva", "mamão"];
+// order.pop(); (remover o ultimo elemento)
+
+// order.push("limão") (adicionar um elemento no final)
+
+// order.shift(); (remover o primeiro elemento)
+
+// order.unshift("pêra"); (adicionar um elemento no começo)
+
+// order [2] = "abacaxi" ; (substituir um item do array -o primeiro elemento do array conta como 0-)
+
+// order[order.length] = caqui; (acessar o tamanho do array e adiconar um item no final)
+
+// delete order[4]; (declarar um item como indefinido)
+
+// order.splice(2,1,"kiwi", "jaca"); (primeiro:posição ; segundo: quantos vão ser substituidos; terceiro, quarto, etc:o que vai substituir aquele item)
+
+// order.splice(0,2); (deleta algum item. primeiro:posição; segundo:quantida de items à serem deletados)
+
+// console.log(order);
+
+
+var girls = ["Ana","Gabi","Bia","Mari"];
+var boys = ["João","Leo", "Caio","Jonas"];
+var teacher = ["clarice","thais", "anderson","alan"];
+
+// var people = girls.concat(boys); (concatenar 2 arrays)
+// console.log(people);
+
+var everyone = girls.concat (boys, teacher);
+console.log(everyone);
